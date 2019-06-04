80489be960d7c3e99a4760040d1e35f9eafc8b94
Juan Carlos Dibot Delgado <juan_dibot@hotmail.com>
2019-06-04 20:18:03 -0300

    Manejador de archivo
    Manejador

--- Security/src/security/Security.java
+++ Security/src/security/Security.java
@@ -10,11 +10,41 @@ public class Security {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String originalString = "ManyaCampeon";
-        System.out.println("Cadena Original - " + originalString);
-        String encryptedString = CBCEncryption.encrypt(originalString);
-        System.out.println("Cadena Encriptada - " + encryptedString);
-        String decryptedString = CBCEncryption.decrypt(encryptedString);
-        System.out.println("Después de la desencriptación - " + decryptedString);
+        //String originalString = "ManyaCampeon";
+        
+        String [] lineasArchivo = ManejadorArchivosGenerico.leerArchivo("src/Security/Prueba1.txt");
+        String cadenaOriginal = "";
+        String resultadoEncriptado = "";
+        String resultadoDesEncriptado = "";
+        
+        
+        for (int j = 0; j< lineasArchivo.length; j++){
+            String[] lineas = lineasArchivo[j].split(" ");
+            
+            for (int i=0; i<lineas.length; i++){
+                String originalString = lineas[i];
+                
+                cadenaOriginal += originalString + " ";
+                String encryptedString = CBCEncryption.encrypt(originalString);
+                resultadoEncriptado = resultadoEncriptado + encryptedString;
+                
+                String decryptedString = CBCEncryption.decrypt(encryptedString);
+                resultadoDesEncriptado = resultadoDesEncriptado + decryptedString + " ";
+            }
+                
+        }
+        
+        
+        System.out.println("La cadena original es: " + cadenaOriginal);
+        System.out.println("El resultado encriptado es: " + resultadoEncriptado);
+        System.out.println("El resultado desencriptado es: " + resultadoDesEncriptado);
+        
+        
+        
+        //System.out.println("Cadena Original - " + originalString);
+        //String encryptedString = CBCEncryption.encrypt(originalString);
+        //System.out.println("Cadena Encriptada - " + encryptedString);
+        //String decryptedString = CBCEncryption.decrypt(encryptedString);
+        //System.out.println("Después de la desencriptación - " + decryptedString);
     }
 }
