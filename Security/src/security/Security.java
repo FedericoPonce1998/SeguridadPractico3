682c9f8b7fa58946d9f1e9801faa75c3522bd85a
Juan Carlos Dibot Delgado <juan_dibot@hotmail.com>
2019-06-04 19:10:22 -0300

    Manejador
    Manejador

--- Security/src/security/Security.java
+++ Security/src/security/Security.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package security;
 
 /**
@@ -15,12 +10,11 @@ public class Security {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String originalString = "password";
-        System.out.println("Original String to encrypt - " + originalString);
+        String originalString = "ManyaCampeon";
+        System.out.println("Cadena Original - " + originalString);
         String encryptedString = CBCEncryption.encrypt(originalString);
-        System.out.println("Encrypted String - " + encryptedString);
+        System.out.println("Cadena Encriptada - " + encryptedString);
         String decryptedString = CBCEncryption.decrypt(encryptedString);
-        System.out.println("After decryption - " + decryptedString);
+        System.out.println("Después de la desencriptación - " + decryptedString);
     }
-    
 }
