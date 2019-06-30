36d2a804fcfba188fdd157419a2f1e12332a1bbc
mariaparapar <>
2019-06-30 17:57:01 -0300

    Estilos

--- SecurityProject/src/java/firmaCifradoDescifrado.java
+++ SecurityProject/src/java/firmaCifradoDescifrado.java
@@ -18,7 +18,7 @@ import javax.xml.bind.DatatypeConverter;
 
 /**
  *
- * @author ceibal
+ * @author Noe
  */
 @WebServlet(urlPatterns = {"/firmaCifradoDescifrado"})
 public class firmaCifradoDescifrado extends HttpServlet {
